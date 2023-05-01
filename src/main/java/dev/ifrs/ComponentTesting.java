5897ea907a1c6e1af4279e5a6b2ac07d8e6c0bd3
taisegoulart <taise.m.goulart@gmail.com>
2023-04-30 23:28:40 -0300

    Adicionando mais testes

--- src/main/java/dev/ifrs/ComponentTesting.java
+++ src/main/java/dev/ifrs/ComponentTesting.java
@@ -7,15 +7,21 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+
 @Path("/coupon")
 public class ComponentTesting {
 
     @POST
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Produces(MediaType.TEXT_PLAIN)
-    public String calculateCoupon(@FormParam("value") float valor, @FormParam("coupon") String coupon) {
-        return "103521";
-       //TODO implementar o método
-       //TODO: praticar test-driven-development! --> ver fotos: situações de erro, de número negativo...
+    public String calculateCoupon(@FormParam("value") float value, @FormParam("coupon") String coupon){
+        if(coupon.equals("CUPOM10")){
+            float withDiscount = Math.round(value * 90.0f);
+            return String.format("%.0f", withDiscount);
+        } else if (coupon.equals("CUPOM15")) {
+            float withDiscount = Math.round(value * 85.0f);
+            return String.format("%.0f", withDiscount);
+        }
+        return "fail";
     }
 }
--- src/test/java/dev/ifrs/ComponentTestingTest.java
+++ src/test/java/dev/ifrs/ComponentTestingTest.java
@@ -1,6 +1,8 @@
 package dev.ifrs;
 
 import io.quarkus.test.junit.QuarkusTest;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -22,6 +24,7 @@ public class ComponentTestingTest {
     } */
 
     @Test
+    @DisplayName("Cupom 10% off")
     public void testCoupon(){
         given()
         .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED)
@@ -29,6 +32,57 @@ public class ComponentTestingTest {
         .when().post("/coupon").then()
            .statusCode(200)
            .body(is("103521"));  
+    }
+    @Test
+    @DisplayName("Cupom 15% off")
+    public void testCoupon15() {
+        given()
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED)
+            .body("value=1150.23&cupom=COUPON15")
+            .when()
+                .post("/coupon")
+            .then()
+                .statusCode(200)
+                .body(is("97770"));
+    }
+
+    @Test
+    @DisplayName("Cupom 100% off")
+    public void testCoupon100() {
+        given()
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED)
+            .body("value=1150.23&cupom=COUPON100")
+            .when()
+                .post("/coupon")
+            .then()
+                .statusCode(200)
+                .body(is("0"));
+    }
+
+
+    @Test
+    @DisplayName("Valor nulo")
+    public void testValueNull() {
+        given()
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED)
+            .body("valor=0&cupom=CUPOM10")
+            .when()
+                .post("/cupom")
+            .then()
+                .statusCode(200)
+                .body(is("0"));
+    }
+
+    @Test
+    @DisplayName("Valor caractere inválido")
+    public void testValueInvalidAlphabetic() {
+        given()
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED)
+            .body("valor=AAA&cupom=CUPOM10")
+            .when()
+                .post("/cupom")
+            .then()
+                .statusCode(400);
     }
 
 }
